3c7508a84e5d3393962b52edecfafb159a3dc1db
NishaM2 <nisham2522@gmail.com>
2025-03-06 20:25:50 +0530

    grid

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -5,19 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div style={{ display: "flex", justifyContent: "center"}}>
-        <div style={{backgroundColor: "red"}}>tailwind</div>
-        <div style={{backgroundColor: "green"}}>taillll</div>
-        <div style={{backgroundColor: "orange"}}>windddd</div>
-      </div>
-
-      <div className='flex justify-center'>
-        <div className='bg-pink-500'>hellooo</div>
-        <div className='bg-yellow-500'>hellooo</div>
-        <div className='bg-blue-500'>hellooo there</div>
-      </div>
-    </>
+   <div className='grid grid-cols-3'>
+    <div className='bg-purple-500'>helloooo</div>
+    <div className='bg-pink-500'>helloooo</div>
+    <div className='bg-blue-500'>helloooo</div>
+   </div>
   )
 }
 
