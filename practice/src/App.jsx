75daba41e0ca49f94b0f8a1133e3946af0998665
NishaM2 <nisham2522@gmail.com>
2025-03-06 20:42:50 +0530

    responsiveness

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className='grid grid-cols-3'>
-    <div className='bg-purple-500'>helloooo</div>
-    <div className='bg-pink-500'>helloooo</div>
-    <div className='bg-blue-500'>helloooo</div>
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-red-500'>hi</div>
+      <div className='bg-pink-500'>hi</div>
+      <div className='bg-blue-500'>hi</div>
    </div>
   )
 }
